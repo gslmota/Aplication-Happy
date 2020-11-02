a65aba7817a9332cae133bb8a88f6e2f768f3a61
Gabriel Sávio <gabrielsavio81@gmail.com>
2020-11-02 15:24:51 -0300

    Update page-orphanage.js

--- public/scripts/page-orphanage.js
+++ public/scripts/page-orphanage.js
@@ -3,3 +3,4 @@ const map = L.map('mapid').setView([51.505, -0.09], 13);
 L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
     attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
 }).addTo(map);
+// falta colocar o mapa
